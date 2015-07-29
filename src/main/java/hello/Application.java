deb426328959111970fe272a70f65a5b74be1fa7
Nabiha makarim Yusuf <nabyusuf@Nabihas-MacBook-Pro.local>
2015-07-29 09:49:59 -0600

    first commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,23 @@
+/*
+package hello;
+
+*/
+/**
+ * Created by nabyusuf on 7/28/15.
+ *//*
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
+
+*/
---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,24 @@
+/*
+package hello;
+
+*/
+/**
+ * Created by nabyusuf on 7/28/15.
+ *//*
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}*/
